358ed398365e70f86230fb456b18e32dafcecee7
iambalakrishnan <imbkrishnaa@gmail.com>
2022-10-12 14:49:58 +0530

    begining of stage 02

---
+++ src/stage_02_prepare_base_model.py
@@ -0,0 +1,37 @@
+import argparse
+import os
+import shutil
+from tqdm import tqdm
+import logging
+from src.utils.common import read_yaml, create_directories
+import random
+
+
+STAGE = "TWO" ## <<< change stage name 
+
+logging.basicConfig(
+    filename=os.path.join("logs", 'running_logs.log'), 
+    level=logging.INFO, 
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
+    filemode="a"
+    )
+
+
+def prepare_base_model(config_path):
+    pass
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configs/config.yaml")
+    #args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info("\n********************")
+        logging.info(f">>>>> stage {STAGE} started <<<<<")
+        prepare_base_model(config_path=parsed_args.config)
+        logging.info(f">>>>> stage {STAGE} completed!, base model is created and saved successfully <<<<<\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
