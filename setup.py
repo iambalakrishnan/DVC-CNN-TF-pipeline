2f53db799dabf14e188a071d5a0a236e498cfaf8
iambalakrishnan <imbkrishnaa@gmail.com>
2022-10-13 17:48:59 +0530

    joblib added

--- setup.py
+++ setup.py
@@ -8,15 +8,17 @@ REPO_NAME = "DVC-CNN-TF-pipeline"
 AUTHOR_USER_NAME = "iambalakrishnan"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = [
+    "pandas==1.3.5",
     "tqdm==4.64.1",
     "dvc==2.10.2",
-    "tensorflow==2.10.0"
+    "tensorflow==2.10.0",
+    "joblib==1.2.0"
 ]
 
 
 setup(
     name=SRC_REPO,
-    version="0.0.2",
+    version="0.0.3",
     author=AUTHOR_USER_NAME,
     description="A small package for DVC",
     long_description=long_description,
